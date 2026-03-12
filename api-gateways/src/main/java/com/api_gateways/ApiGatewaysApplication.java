840022a6c0a52a398aaa23a7f4a9b1d152ea5d73
mahaksarathe-18 <sarathemahak0@gmail.com>
2026-03-12 23:30:24 +0530

    adding api-gateway

---
+++ api-gateways/src/main/java/com/api_gateways/ApiGatewaysApplication.java
@@ -0,0 +1,15 @@
+package com.api_gateways;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ApiGatewaysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewaysApplication.class, args);
+	}
+
+}
